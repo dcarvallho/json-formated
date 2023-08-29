e503cd6d130df09818f13320a5bc6acf855e5bac
Davi Augusto <davi_augusto11@hotmail.com>
2023-08-29 00:06:29 -0300

    Cria card de menu

---
+++ components/card-menu/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+const Menu = () =>{
+    return(
+        <>
+            <div className={styles.backgroundMenu}>
+                <div className={styles.cardMenu}>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Menu;
